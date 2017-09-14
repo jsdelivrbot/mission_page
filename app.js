e977653cc4d39c292f216cd34fddf946b6086117
JY Cheah <JY Cheah>
2017-09-14 16:35:18 +0800

    a bit messed up newmission page

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ expressSanitizer = require("express-sanitizer"),
 express = require("express"),
 app = express();
 
+var async = require('async');
 var moment = require('moment');
-moment().format();
+// moment().format();
 
 // APP CONFIG
 mongoose.connect("mongodb://localhost/missions");
@@ -22,6 +23,7 @@ var missionSchema = new mongoose.Schema({
     title: String,
     image: {type: String, default: "https://12obtk2jn55z2ec8wk2hgcfw2-wpengine.netdna-ssl.com/wp-content/uploads/2017/03/Mission.png"},
     details: String,
+    category: String,
     reward: String,
 
     status: String,
@@ -56,25 +58,53 @@ var userSchema = new mongoose.Schema({
 var User = mongoose.model("User", missionSchema);
 
 
-Mission.find().remove().exec(function(err, data) {
-  // data will equal the number of docs removed, not the document itself
-});
+// Mission.find().remove().exec(function(err, data) {
+//   // data will equal the number of docs removed, not the document itself
+// });
 
 // RESTFUL ROUTES
 app.get("/", function(req, res){
     res.redirect("/missions");
 });
 
-app.get("/missions", function(req, res){
-    Mission.find({}, function(err, missions){
-        if(err){
-            console.log("ERROR!");
-        }else{
-            // var hi = moment(missions.date_created).fromNow()
-            // console.log(hi);
-            res.render("index", {missions: missions, moment:moment});
+app.get("/missions/NewMission", function(req, res){
+	Mission.find({status: "NEW"}, function(err, nM){
+    		// console.log(nM);
+	        if(err){
+	            console.log("ERROR!");
+	        }else{
+	 
+
+            res.render("NewMission", {nM: nM, moment:moment});
         }
-    })  
+    });  
+
+});
+
+//INDEX
+app.get("/missions", function(req, res){
+
+	// missions.$where({status: "NEW"})
+	// var newMission = Mission.find().where({status: 'NEW'});
+	
+		var nM = Mission.find({status: "NEW"}, function(err, value){
+    			return(value);
+    	})  
+    	var oG = Mission.find({status: "ONGOING"}, function(err, value){
+    			return(value);
+    	})  
+    
+
+    Mission.find({}, function(err, missions){
+    		// console.log(nM);
+	        if(err){
+	            console.log("ERROR!");
+	        }else{
+	 
+
+            res.render("index", {missions: missions, nM: nM, oG: oG, moment:moment});
+        }
+    });  
 });
 
 // NEW ROUTE
@@ -90,8 +120,10 @@ app.get("/missions/new", function(req, res){
 app.post("/missions", function(req, res){
     //create blog
     req.body.mission.details = req.sanitize(req.body.mission.details);
+
+
     // req.body.mission.image = "https://12obtk2jn55z2ec8wk2hgcfw2-wpengine.netdna-ssl.com/wp-content/uploads/2017/03/Mission.png";
-    req.body.mission.status = "Open";
+    req.body.mission.status = "NEW";
     Mission.create(req.body.mission, function(err, newMission){
         if(err){
             res.render("new");
